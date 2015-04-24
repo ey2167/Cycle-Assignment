95b856cce34fb0f156520b186475d4489ed9cd7d
ey2167 <fzreo338@gmail.com>
2015-04-24 16:37:03 -0700

    cycle for unit7

--- Cycle/src/Cycle.java
+++ Cycle/src/Cycle.java
@@ -1,22 +1,42 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 
 public class Cycle {
+	double NumberOfWheels;
 	/**
 	 * initializing variables for constructor class Cycle, and making the variables
 	 * private so they cannot be called in other classes normally
 	 */
-private int NumberOfWheels, weight;
-
+private double weight;
+Scanner keyboard = new Scanner(System.in);
 public Cycle(){
-	this(100,1000);
+	this(100.0,1000.0);
 	
 	/**
 	 * this method will give default values to the numberofwheels and weight
 	 */
 }
 
-public Cycle(int NumberOfWheels, int weight){
+public Cycle(double NumberOfWheels, double weight){
+	try{
+	System.out.println("type number of wheels here");
+	NumberOfWheels= keyboard.nextDouble();
+	System.out.println("type weight here");
+	weight = keyboard.nextDouble();
 	this.NumberOfWheels = NumberOfWheels;
 	this.weight= weight;
+	}
+	catch(Exception e){
+		System.out.println("invalid input, closing");
+		System.exit(0);
+	}
+	if(weight<0 || NumberOfWheels<0){
+		throw new IllegalArgumentException("no negative weight, closing");
+		
+			
+		
+	}
 	/**
 	 * the this values here will set the private variables equal to the parameters of the constructor
 	 */
